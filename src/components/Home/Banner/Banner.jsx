23977049afea9c62286fcd2699377a363113974b
munna mahmud <learnermunna@gmail.com>
2022-01-27 15:37:31 +0600

    banner typewriter done and work on font-family

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Banner.css';
+import { Typewriter } from 'react-simple-typewriter'
 // import { Row, Col } from 'react-bootstrap';
 // import banner from ''
 
@@ -9,9 +10,24 @@ const Banner = () => {
             <div class="row">
                 <div class="col-md-6 text-start mt-5 pt-5">
                     <div className='pt-5 banner'>
-                        <h1 className='name_title'>MUNNA BABU</h1>
-                        <h3 className="pb-4 text-secondary">Creative Front-End Developer</h3>
-                        <button className='pt-3 pb-3 ' ><a className='ps-2 pe-2'  target="_blank" href="https://drive.google.com/file/d/16GsHo8Ze-kMyPLB3QuCgTdXXt9bWVAsr/view?usp=sharing"><i class="fas fa-arrow-down"></i> DOWNLOAD RESUME</a></button>
+                        <h1 className='name_title' style={{fontFamily:"Poppins"}}>MUNNA BABU</h1>
+                        <h3 className="pb-4 " style={{color:'#9B9BA3'}}>Creative
+                            <span style={{ color: 'black', fontWeight: 'bold' }}>
+                                {/* Style will be inherited from the parent element */}
+                                <Typewriter
+                                    words={[' Front-End Developer', ' UI Designer', ' MERN Stack Developer']}
+                                    cursor
+                                    cursorStyle='_'
+                                    typeSpeed={70}
+                                    loop={10}
+                                    deleteSpeed={50}
+                                    delaySpeed={1000}
+
+                                />
+                            </span>
+
+                        </h3>
+                        <button className='pt-3 pb-3 ' ><a className='ps-2 pe-2' target="_blank" href="https://drive.google.com/file/d/16GsHo8Ze-kMyPLB3QuCgTdXXt9bWVAsr/view?usp=sharing"><i class="fas fa-arrow-down"></i> DOWNLOAD RESUME</a></button>
                     </div>
                 </div>
                 <div class="col-md-6  text-end pb-4 pt-4 mt-5">
