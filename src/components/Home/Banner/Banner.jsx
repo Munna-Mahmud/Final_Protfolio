7ca18f10c2684b5b50aa5833637d51421232c44c
munna mahmud <learnermunna@gmail.com>
2022-01-31 21:27:14 +0600

    about content change

--- src/components/Home/Banner/Banner.jsx
+++ src/components/Home/Banner/Banner.jsx
@@ -6,34 +6,42 @@ import { Typewriter } from 'react-simple-typewriter'
 
 const Banner = () => {
     return (
-        <div className='ms-4 me-4 '>
-            <div className="row" data-aos="fade-up">
-                <div className="col-md-6 text-start mt-5 pt-5">
-                    <div className='pt-5 banner'>
-                        <h1 className='name_title' style={{fontFamily:"Poppins"}}>MUNNA BABU</h1>
-                        <h3 className="pb-4 " style={{color:'#9B9BA3'}}>Creative
-                            <span style={{ color: 'black', fontWeight: 'bold' }}>
-                                {/* Style will be inherited from the parent element */}
-                                <Typewriter
-                                    words={[' Front-End Developer', ' UI Designer', ' MERN Stack Developer']}
-                                    cursor
-                                    cursorStyle='_'
-                                    typeSpeed={70}
-                                    loop={10}
-                                    deleteSpeed={50}
-                                    delaySpeed={1000}
+        <div className="container-fluid ">
+            <div className="row" >
+                <div className="col-md-6 col-xl-6 col-sm-12" >
+                    <div className=" text-start mt-5 pt-5">
+                        <div className='pt-5 banner'>
+                            <h1 className='name_title bg-xl-primary bg-md-danger bg-sm-secondary' style={{ fontFamily: "Poppins" }}>MUNNA BABU</h1>
+                            <h3 className="pb-4 " style={{ color: '#9B9BA3' }}>Creative
+                                <span style={{ color: 'black', fontWeight: 'bold' }}>
+                                    {/* Style will be inherited from the parent element */}
+                                    <Typewriter
+                                        words={[' Front-End Developer', ' UI Designer', ' MERN Stack Developer']}
+                                        cursor
+                                        cursorStyle='_'
+                                        typeSpeed={70}
+                                        loop={10}
+                                        deleteSpeed={50}
+                                        delaySpeed={1000}
 
-                                />
-                            </span>
+                                    />
+                                </span>
 
-                        </h3>
-                        <button className='pt-3 pb-3 ' ><a className='ps-2 pe-2' target="_blank" href="https://drive.google.com/file/d/16GsHo8Ze-kMyPLB3QuCgTdXXt9bWVAsr/view?usp=sharing"><i className="fas fa-arrow-down"></i> DOWNLOAD RESUME</a></button>
+                            </h3>
+                            <button className='pt-3 pb-3 ' ><a className='ps-2 pe-2' target="_blank" href="https://drive.google.com/file/d/16GsHo8Ze-kMyPLB3QuCgTdXXt9bWVAsr/view?usp=sharing"><i className="fas fa-arrow-down"></i> DOWNLOAD RESUME</a></button>
+                        </div>
                     </div>
+
                 </div>
-                <div className="col-md-6  text-end pb-4 pt-4 mt-5">
-                    <div className="banner_img text-end">
-                        {/* img  */}
+
+                <div className="col-md-6 col-xl-6 col-sm-12">
+
+                    <div className=" text-end pb-4 pt-4 mt-5">
+                        <div className="banner_img text-end">
+                            {/* img  */}
+                        </div>
                     </div>
+
                 </div>
             </div>
         </div>
